f9b9d9cc0c9b0aa4f8c1975a160d4216e4a0763f
Sherzel Smith <sherzelsmith@gmail.com>
2017-10-20 10:34:04 +0000

    Added places edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,9 +16,12 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place =Place.find(params[:id])
+    @place = Place.find(params[:id])
   end 
 
+  def edit
+    @place= Place.find(params[:id])
+  end
 
   private
 
