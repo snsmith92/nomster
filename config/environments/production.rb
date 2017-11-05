331e64f8554e61f36b9bfd646208b853ff8d58e6
Sherzel Smith <sherzelsmith@gmail.com>
2017-11-05 12:25:10 +0000

    Added sendgrid to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,18 @@
 Rails.application.configure do
+
+  # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
   config.action_mailer.default_url_options = { host: 'nomster-sherzel-smith.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
